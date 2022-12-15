701470bea71c46cd3bdb80fdb5f7fec174addfec
Sanoop <89683314+sanoop7@users.noreply.github.com>
2022-12-16 04:57:13 +0530

    llll

--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -4,6 +4,10 @@ import React from "react";
 import "./projects.css";
 function Projects() {
  
+  function projectShow(id) {
+    console.log(id);
+    document.getElementById(id).classList.add('project_item_active');
+  }
   function OpenNewTab(params) {
     params === "git"
       ? window.open("https://github.com/sanoop7/Tictacktoe", "_blank")
@@ -15,7 +19,7 @@ function Projects() {
       <span className="section_1_head">Projects</span>
       <div className="project_main">
        <div className="projects_main_list">
-        <div className="project_item">
+        <div id="project_item_1" className="project_item" onClick={(e) => projectShow(e.target.id)}>
           <div className="project_links">
             <div className="demo_link" onClick={() => OpenNewTab("demo")}><img className="project_links_icon" alt="pro_pic" src={linkIcon}  />Demo</div>
             <div className="repo_link" onClick={() => OpenNewTab("git")}><img className="project_links_icon" alt="pro_pic" src={git} />Repo</div>
